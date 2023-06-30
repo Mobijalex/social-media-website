0461d22f7e534e8b24ecb841d0e66de19b2f5466
mobijalex <mobijalex6@gmail.com>
2023-06-30 19:26:44 +0530

    javascript bug

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 const menuItem = document.querySelectorAll(".menu-item");
 
+// MESSAGES
+
+const messagesNotification = document.querySelector("#messages-notification");
+const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
+
 // REMOVE ACTIVE CLASS FROM ALL MENU ITEM
 
 const changeActiveItem = () => {
@@ -9,6 +16,7 @@ const changeActiveItem = () => {
     item.classList.remove("active");
   });
 };
+//  NOTIFICATION POPUP NOT WORKING
 
 menuItem.forEach((item) => {
   item.addEventListener("click", () => {
@@ -21,10 +29,39 @@ menuItem.forEach((item) => {
       document.querySelector(".notification-popup");
       style.display = "block";
       document.querySelector(
-        "#notification . notification-count"
+        "#notification .notification-count"
       ).style.display = "none";
     }
   });
 });
 
-// NOT WORKING
+//  NOTIFICATION POPUP NOT WORKING
+
+// MESSAGES SECTION STARTS
+
+// SEARCH CHAT OPTION
+const searchMessage = () => {
+  const val = messageSearch.value.toLoweCase();
+  message.forEach((chat) => {
+    let name = chat.querySelectorAll("h5").textContent.toLowerCase;
+    if (name.indexOf(val) != -1) {
+      user.style.display = "flex";
+    } else {
+      user.style.display = "none";
+    }
+  });
+};
+
+messageSearch.addEventListener("keyup , searchMessage");
+
+// MESSAGES SECTION ENDS NOT WORKING
+
+messagesNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var (--color-primary)";
+  // MESSAGES  BOX SHADOW NOT WORKING
+  messagesNotification.querySelector(".notification-count").style.display =
+    "none";
+  setTimeout(() => {
+    messages.style.boxShadow = "none";
+  }, 2000);
+});
