91565ef76a8bec37f03a13610076c3c92b9ee8ea
Mobijalex <mobijalex6@gmail.com>
2023-07-06 12:57:47 +0530

    small js bugs

--- index.js
+++ index.js
@@ -140,10 +140,79 @@ fontSizes.forEach(size => {
 
 // CHANGE PRIMARY COLOURS
 
-const colorPalette = document.querySelectorAll('choose-color span')
+const colorPalette = document.querySelectorAll('.choose-color span')
 
 colorPalette.forEach(color => {
   color.addEventListener('click', () => {
-    
+    let primary;
+
+    if(color.classList.contains('color-1')){
+      primaryHue = 252;
+    } else if (color.classList.contains('color-2')){
+      primaryHue = 52;
+    } else if (color.classList.contains('color-3')){
+      primaryHue = 352;
+    } else if (color.classList.contains('color-4')){
+      primaryHue = 152;
+    } else if (color.classList.contains('color-5')){
+      primaryHue = 202;
+    }
+    color.classList.add('active');
+    root.style.setProperty('--primary-color-hue', primaryHue)
   })
 })
+
+// BACKGROUND COLOR SECTION STARTS HERE
+
+const Bg1 = document.querySelector('.bg-1')
+const Bg2 = document.querySelector('.bg-2')
+const Bg3= document.querySelector('.bg-3')
+
+
+// THEME BACKGROUND VALUES
+let lightColorLightness;
+let WhiteColorLightness;
+let DarkColorLightness;
+
+
+// CHANGE BACKGROUND FUNCTIONS 
+
+const changeBG = () => {
+  root.style.setProperty('--light-color-lightness',lightColorLightness)
+  root.style.setProperty('--white-color-lightness',WhiteColorLightness)
+  root.style.setProperty('--dark-color-lightness',DarkColorLightness)
+}
+
+
+// THEME BACKGROUND VALUES
+
+Bg1.addEventListener('click', () => {
+  Bg1.classList.add('active');
+  Bg2.classList.remove('active');
+  Bg3.classList.remove('active');
+  window.location.reload();
+})
+
+
+Bg2.addEventListener('click',() => {
+  DarkColorLightness = '95%';
+  WhiteColorLightness = '20%';
+  lightColorLightness = '15%';
+
+  Bg2.classList.add('active');
+  Bg1.classList.remove('active');
+  Bg3.classList.remove('active');
+  changeBG();
+});
+
+
+Bg3.addEventListener('click',() => {
+  DarkColorLightness = '95%';
+  WhiteColorLightness = '10%';
+  lightColorLightness = '0%';
+
+  Bg3.classList.add('active');
+  Bg1.classList.remove('active');
+  Bg1.classList.remove('active');
+  changeBG();
+});
